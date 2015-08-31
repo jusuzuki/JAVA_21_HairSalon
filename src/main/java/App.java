b28b616bf39947ffa03efcba6556c975d06a1477
Juliana Suzuki <jsayumi@gmail.com>
2015-08-31 12:05:27 -0700

    Add methods to create new stylists and clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,22 +13,66 @@ public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
-    //homepage
+    //HOMEPAGE
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
 
       //show all stylists
-      // List<Stylist> stylists = Stylist.all();
-      // model.put("stylists", stylists);
+      List<Stylist> stylists = Stylist.all();
+      model.put("stylists", stylists);
 
       //show all clients
-      // List<Client> clients = Client.all();
-      // model.put("clients", clients);
+      List<Client> clients = Client.all();
+      model.put("clients", clients);
 
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+
+    //ADD NEW STYLIST
+    post("/stylists/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      //add new stylist
+      String newStylistName = request.queryParams("newStylistName");
+      Stylist newStylist = new Stylist(newStylistName);
+      newStylist.save();
+
+      //show all stylists
+      List<Stylist> stylists = Stylist.all();
+      model.put("stylists", stylists);
+
+      //show all clients
+      List<Client> clients = Client.all();
+      model.put("clients", clients);
+
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    //ADD NEW CLIENT
+    post("/clients/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      //add new client
+      String newClientName = request.queryParams("newClientName");
+      int clientStylistId = Integer.parseInt(request.queryParams("clientStylist"));
+      Client newClient = new Client(newClientName, clientStylistId);
+      newClient.save();
+
+      //show all stylists
+      List<Stylist> stylists = Stylist.all();
+      model.put("stylists", stylists);
+
+      //show all clients
+      List<Client> clients = Client.all();
+      model.put("clients", clients);
+
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
  }
 }
